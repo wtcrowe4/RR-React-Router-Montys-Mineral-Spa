7e00cfd0220943bb707e7b2111f021f264b36fab
Thomas Crowe <wcrowe@g.clemson.edu>
2022-08-17 19:40:03 -0400

    Worked through activity

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom'
 
 import './App.css';
 
@@ -12,24 +13,33 @@ function App() {
 
   return (
     <div className="App">
-      <header>
-        <h1 className="title">Welcome to Monty's Mineral SPA</h1>
+      <Router>
+        <header>
+          <h1 className="title">Welcome to Monty's Mineral SPA</h1>
 
-        <div className="navBar">
-          <ul>
-            <li>
-              <a href={<Home />}>Home</a>
-            </li>
-            <li>
-              <a href={<About />}>About Us</a>
-            </li>
-            <li>
-              <a href={<Packages packages={packages}/>}>Our Packages</a>
-            </li>
-          </ul>
+          <div className="navBar">
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About Us</Link>
+              </li>
+              <li>
+                <Link to="/packages">Our Packages</Link>
+              </li>
+            </ul>
+          </div>
+        </header>
+
+        <div className="display">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/packages" element={<Packages packages={packages} />} />
+          </Routes>
         </div>
-
-      </header>
+      </Router>
     </div>
   );
 }
--- react-router/src/components/Packages.js
+++ react-router/src/components/Packages.js
@@ -1,14 +1,16 @@
 
 
-export default function Packages() {
-
+export default function Packages(props) {
+    const packages = props.packages.map(pack => <li>{pack}</li>);
     return (
         <div>
             <div className="packages">
                 <div className="packagesHeader">
                     <h1>Our Packages</h1>
                 </div>
-                
+                <ul>
+                    {packages}
+                </ul>
             </div>
         </div>
     )
